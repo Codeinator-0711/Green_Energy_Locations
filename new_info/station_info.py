8d64ff4e0ea891ca5e594ef27bc1f8d942463212
Soenke <soenkemaibach@gmail.com>
2021-08-22 20:11:41 +0200

    new_info notebook unpacks coordinates into new cols

--- new_info/station_info.py
+++ new_info/station_info.py
@@ -11,8 +11,11 @@ def addinfo(weather_df, station_df, add):
     if not station_df.index.name == 'id':
         if not 'id' in cols:
             cols.append('id')
-    to_join = station_df[cols]
+    to_join = station_df[cols].copy()
     if 'id' in to_join.columns:
         to_join.set_index('id',inplace = True, drop = True)
+    if 'location' in cols:
+        to_join.loc[:,'latitude'] = [eval(x)[0] for x in to_join.location]
+        to_join.loc[:,'longitude'] = [eval(x)[1] for x in to_join.location]
     added_df = pd.merge(weather_df,to_join,left_on='id',right_on='id')
     return added_df
